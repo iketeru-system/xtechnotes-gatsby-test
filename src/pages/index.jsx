5e23781ffc42ebc94cc61f89586ceacc0dabd5b5
Yuto Ono <stellarwindmaster@gmail.com>
2022-05-19 19:04:14 +0900

    set favicon

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,12 @@
 import Helmet from 'react-helmet'
+import favicon from '../images/favicon.png'
 
 const IndexPage = () => {
   return (
     <>
       <Helmet>
         <title>【体験談】プログラミング系の職業訓練校｜5週間目 | Xtechのーと</title>
+        <link rel='icon' type='image/png' href={favicon} />
       </Helmet>
       <div>Xtechnotes Gatsby Test</div>
     </>
