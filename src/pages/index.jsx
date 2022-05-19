f115becb5b014c089bacf20aa2b5e29fdb7d0103
Yuto Ono <stellarwindmaster@gmail.com>
2022-05-19 21:57:32 +0900

    create Header

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,5 @@
 import Helmet from 'react-helmet'
+import Header from '../components/Header'
 import favicon from '../images/favicon.png'
 
 const IndexPage = () => {
@@ -9,7 +10,7 @@ const IndexPage = () => {
         <link rel='icon' type='image/png' href={favicon} />
         <meta name='robots' content='noindex' />
       </Helmet>
-      <div>Xtechnotes Gatsby Test</div>
+      <Header />
     </>
   )
 }
