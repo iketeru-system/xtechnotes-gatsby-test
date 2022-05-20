44e6f99281bc62f63827840885786c7e99fbd463
Yuto Ono <stellarwindmaster@gmail.com>
2022-05-20 19:15:10 +0900

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,34 @@
+import { StaticImage } from 'gatsby-plugin-image'
+
 const Header = () => {
   return (
-    <header />
+    <header
+      css={{
+        boxShadow: '0 1px 4px rgb(0 0 0 / 12%)'
+      }}
+    >
+      <div
+        css={{
+          maxWidth: '1296px',
+          margin: '0 auto',
+          padding: '0 48px'
+        }}
+      >
+        <div
+          css={{
+            padding: '16px 0'
+          }}
+        >
+          <StaticImage
+            src='../images/headerlogo.jpg'
+            layout='fixed'
+            width={178}
+            height={40}
+            alt='Xtechのーと'
+          />
+        </div>
+      </div>
+    </header>
   )
 }
 
