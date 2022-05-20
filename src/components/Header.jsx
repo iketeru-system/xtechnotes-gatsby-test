696d303d978621900e19419c08b83ba6106ac10c
Yuto Ono <stellarwindmaster@gmail.com>
2022-05-20 20:43:22 +0900

    Header レスポンシブ化

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,12 @@
 import { StaticImage } from 'gatsby-plugin-image'
+import { css } from '@emotion/react'
+import { mq } from '../utilities/styleUtilities'
 
 const Header = () => {
   return (
-    <header
-      css={{
-        boxShadow: '0 1px 4px rgb(0 0 0 / 12%)'
-      }}
-    >
-      <div
-        css={{
-          maxWidth: '1296px',
-          margin: '0 auto',
-          padding: '0 48px'
-        }}
-      >
-        <div
-          css={{
-            padding: '16px 0'
-          }}
-        >
+    <header css={styles.header}>
+      <div css={styles.inner}>
+        <div css={styles.logo}>
           <StaticImage
             src='../images/headerlogo.jpg'
             layout='fixed'
@@ -33,3 +21,27 @@ const Header = () => {
 }
 
 export default Header
+
+const styles = {
+  header: css`
+    box-shadow: 0 1px 4px rgb(0 0 0 / 12%);
+  `,
+
+  inner: css`
+    max-width: 1296px;
+    margin: 0 auto;
+    padding: 0 48px;
+    ${mq.mdDown} {
+      padding: 0;
+      display: flex;
+      justify-content: center;
+    }
+  `,
+
+  logo: css`
+    padding: 16px 0;
+    ${mq.mdDown} {
+      padding: 10px 0;
+    }
+  `
+}
---
+++ src/utilities/styleUtilities.js
@@ -0,0 +1,7 @@
+// Media Queries
+export const mq = {
+  smDown: '@media (max-width: 599px)',
+  smUp: '@media (max-width: 600px)',
+  mdDown: '@media (max-width: 959px)',
+  mdUp: '@media (min-width: 960px)'
+}
