4979ae9cacafca8e870def5de9cff019e5ac2ef0
Yuto Ono <stellarwindmaster@gmail.com>
2022-05-20 21:18:38 +0900

    Update styleUtilities.js

--- src/utilities/styleUtilities.js
+++ src/utilities/styleUtilities.js
@@ -1,7 +1,7 @@
 // Media Queries
 export const mq = {
   smDown: '@media (max-width: 599px)',
-  smUp: '@media (max-width: 600px)',
+  smUp: '@media (min-width: 600px)',
   mdDown: '@media (max-width: 959px)',
   mdUp: '@media (min-width: 960px)'
 }
