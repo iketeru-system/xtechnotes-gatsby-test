a2fd793c32cb44f10d9be94c7f0f1bc1c164c6d1
Yuto Ono <stellarwindmaster@gmail.com>
2022-05-20 21:07:15 +0900

    Update Content.jsx

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,9 +1,11 @@
+import { css } from '@emotion/react'
+import { mq } from '../utilities/styleUtilities'
 import Main from './Main'
 import Sidebar from './Sidebar'
 
 const Content = () => {
   return (
-    <div>
+    <div css={styles.content}>
       <Main />
       <Sidebar />
     </div>
@@ -11,3 +13,21 @@ const Content = () => {
 }
 
 export default Content
+
+const styles = {
+  content: css`
+    max-width: 1296px;
+    padding: 27px 15px 81px;
+    margin-left: auto;
+    margin-right: auto;
+    ${mq.smUp} {
+      padding: 64px 30px 96px;
+    }
+    ${mq.mdUp} {
+      padding: 64px 48px 96px;
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-between;
+    }
+  `
+}
