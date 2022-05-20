d2f084207f7db9a1f011d533bc595889afe094d7
Yuto Ono <stellarwindmaster@gmail.com>
2022-05-20 21:15:49 +0900

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,26 @@
+import { css } from '@emotion/react'
+import { mq } from '../utilities/styleUtilities'
+
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div css={styles.sidebar}>
+      Sidebar
+    </div>
   )
 }
 
 export default Sidebar
+
+const styles = {
+  sidebar: css`
+    padding: 200px 0;
+    background-color: #ccc;
+    text-align: center;
+    ${mq.mdUp} {
+      width: 304px;
+    }
+    ${mq.mdDown} {
+      margin-top: 54px;
+    }
+  `
+}
