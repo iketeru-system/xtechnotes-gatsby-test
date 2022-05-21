07684d969259bbfb9a5da49fb3ebd2ba9441cf8a
Yuto Ono <stellarwindmaster@gmail.com>
2022-05-21 10:48:54 +0900

    Update Content.jsx

--- src/components/Article/Content.jsx
+++ src/components/Article/Content.jsx
@@ -3,8 +3,8 @@ import ContentContainer from './ContentContainer'
 const Content = () => {
   return (
     <ContentContainer>
-      <p>Paragraph 1</p>
-      <p>Paragraph 2</p>
+      <p>職業訓練校の5週間目はゴールデンウィークの間に実施されました。</p>
+      <p>クラスメイトのうちの何人かともはじめて飲みに行ったので、ゴールデンウィークになにをして過ごしたのかや、クラスメイトについて感じたことなどをまとめておきたいと思います。</p>
     </ContentContainer>
   )
 }
