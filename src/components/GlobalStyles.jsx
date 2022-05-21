92691ce2a8d034bc79d0f3ab6321e109315af30c
Yuto Ono <stellarwindmaster@gmail.com>
2022-05-21 09:01:14 +0900

    Update GlobalStyles.jsx

--- src/components/GlobalStyles.jsx
+++ src/components/GlobalStyles.jsx
@@ -1,29 +1,29 @@
-import { Global } from '@emotion/react'
+import { Global, css } from '@emotion/react'
 
 const GlobalStyles = () => {
   return (
-    <Global
-      styles={{
-        html: {
-          overflowX: 'hidden'
-        },
-        body: {
-          fontFamily: '"游ゴシック体", "Yu Gothic", YuGothic, "Hiragino Kaku Gothic ProN", "Hiragino Sans", "Meiryo", sans-serif',
-          lineHeight: 1.75,
-          boxSizing: 'border-box'
-        },
-        '*': {
-          margin: 0,
-          padding: 0,
-          fontSize: 'inherit',
-          fontWeight: 'inherit'
-        },
-        '*, *::before, *::after': {
-          boxSizing: 'inherit'
-        }
-      }}
-    />
+    <Global styles={styles} />
   )
 }
 
 export default GlobalStyles
+
+const styles = css`
+  html {
+    overflow-x: hidden;
+  }
+
+  body {
+    font-family: "游ゴシック体", "Yu Gothic", YuGothic, "Hiragino Kaku Gothic ProN", "Hiragino Sans", "Meiryo", sans-serif;
+    line-height: 1.6;
+    box-sizing: border-box;
+  }
+
+  * {
+    box-sizing: inherit;
+    margin: 0;
+    padding: 0;
+    font-size: inherit;
+    line-height: inherit;
+  }
+`
