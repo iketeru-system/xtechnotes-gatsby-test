854d016d9b49474c2cf4523692911bcadbf6975e
Yuto Ono <stellarwindmaster@gmail.com>
2022-05-21 18:29:41 +0900

    create Heading2

--- src/components/Article/Content.jsx
+++ src/components/Article/Content.jsx
@@ -1,5 +1,6 @@
 import CapBox from './CapBox'
 import ContentContainer from './ContentContainer'
+import Heading2 from './Heading2'
 
 const Content = () => {
   return (
@@ -7,6 +8,7 @@ const Content = () => {
       <p>職業訓練校の5週間目はゴールデンウィークの間に実施されました。</p>
       <p>クラスメイトのうちの何人かともはじめて飲みに行ったので、ゴールデンウィークになにをして過ごしたのかや、クラスメイトについて感じたことなどをまとめておきたいと思います。</p>
       <CapBox />
+      <Heading2 id='id0'>今週の記録</Heading2>
     </ContentContainer>
   )
 }
