66daff0d4b14c7136cee0ae4523fa0fac0f5b58d
Yuto Ono <stellarwindmaster@gmail.com>
2022-05-21 10:53:39 +0900

    create CabBox

---
+++ src/components/Article/CapBox.jsx
@@ -0,0 +1,21 @@
+const CapBox = () => {
+  return (
+    <div>
+      <div><span>この記事を書いたときの状況</span></div>
+      <div>
+        <div>
+          {/* img */}
+          <div>
+            <ul>
+              <li>2022年から職業訓練校に通っています</li>
+              <li>最近はPHPとJavaScriptをがんばって勉強したい欲が上昇中</li>
+              <li>クラスの雰囲気が好き</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CapBox
--- src/components/Article/Content.jsx
+++ src/components/Article/Content.jsx
@@ -1,3 +1,4 @@
+import CapBox from './CapBox'
 import ContentContainer from './ContentContainer'
 
 const Content = () => {
@@ -5,6 +6,7 @@ const Content = () => {
     <ContentContainer>
       <p>職業訓練校の5週間目はゴールデンウィークの間に実施されました。</p>
       <p>クラスメイトのうちの何人かともはじめて飲みに行ったので、ゴールデンウィークになにをして過ごしたのかや、クラスメイトについて感じたことなどをまとめておきたいと思います。</p>
+      <CapBox />
     </ContentContainer>
   )
 }
