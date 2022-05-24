57a4113e9a90c0224706df9edbe3700ae0189405
Yuto Ono <stellarwindmaster@gmail.com>
2022-05-24 21:54:44 +0900

    Update ProfileBox.jsx

--- src/components/ProfileBox.jsx
+++ src/components/ProfileBox.jsx
@@ -1,7 +1,33 @@
 const ProfileBox = () => {
   return (
     <div>
-      ProfileBox
+      <figure>
+        {/* img */}
+      </figure>
+      <div>あん</div>
+      <div>エンジニア</div>
+      <div>
+        駆け出しエンジニアです。<br /><br />
+        2022年4月から1年間、職業訓練校でHTML・CSS・JavaScript・PHP・Javaの勉強をしています。<br /><br />
+        前職は、英語教材編集者×家庭教師でした。使いやすさ・理解のしやすさ・正確性を考慮した、良質なサービスを提供することを心がけています。
+        <div>
+          <a href='https://xtechnotes.com/profile/'>
+            READ MORE
+          </a>
+        </div>
+      </div>
+      <ul>
+        <li>
+          <a href='https://twitter.com/333ACCC' target='_blank' rel='noopener noreferrer'>
+            <i />
+          </a>
+        </li>
+        <li>
+          <a href='https://xtechnotes.com/feed/' target='_blank' rel='noopener noreferrer'>
+            <i />
+          </a>
+        </li>
+      </ul>
     </div>
   )
 }
